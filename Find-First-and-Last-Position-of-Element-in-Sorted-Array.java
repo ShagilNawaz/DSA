73d88b4c57a64759e1a1d9d2965b0b6df9bae51e
ShagilNawaz <shagilnawaz007@gmail.com>
2025-09-15 17:34:05 +0530

    [34] [Time Beats: 100.00%] [Memory Beats: 48.92%] - LeetPush

--- Find-First-and-Last-Position-of-Element-in-Sorted-Array.java
+++ Find-First-and-Last-Position-of-Element-in-Sorted-Array.java
@@ -1,19 +1,48 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int n = nums.length;
-        int[] arr = {-1,-1};
-        for (int i = 0; i < n; i++){
-            if (nums[i] == target){
-                arr[0] = i;
-                break;
+    public int[] searchRange(int[] arr, int target) {
+        int[] ans = {-1, -1};
+        int s = search(arr, target, true);
+        int e = search(arr, target, false);
+        ans[0] = s;
+        ans[1] = e;
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return ans;
             }
         }
-        for (int j = n - 1; j >= 0; j--){
-            if (nums[j] == target){
-                arr[1] = j;
-                break;
+        return ans;
+    }
+    int search (int[] nums,int target,boolean findStartIndex){
+        int ans = -1;
+        int s = 0;
+        int e = nums.length -1;
+        while (s <= e){
+            int mid = s + (e - s) / 2;
+            if (target < nums[mid]){
+                e = mid -1;
+
+            }
+            else if (target > nums[mid]){
+                s = mid + 1;
+            }
+            else {
+                ans = mid;
+                if (findStartIndex == true){
+                   e = mid - 1;
+                }
+                else {
+                    s = mid + 1;
+                }
             }
         }
-        return arr;
+        return ans;
     }
 }
